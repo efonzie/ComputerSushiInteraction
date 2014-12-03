d012b6a414bf41fb8895516cb58632b18115b7a3
Ethan Matthews <Efonzie@gmail.com>
2014-12-02 22:40:19 -0800

    First Commit

---
+++ gen/com/hci_420_620/ComputerSushiInteraction/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.hci_420_620.computersushiinteraction;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
