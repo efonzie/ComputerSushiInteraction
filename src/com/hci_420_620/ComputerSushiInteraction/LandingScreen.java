18efd558022981050f5eb8be34a7f915857825e0
Ethan Matthews <Efonzie@gmail.com>
2014-12-06 21:41:19 -0800

    Fixed imports

--- src/com/hci_420_620/ComputerSushiInteraction/LandingScreen.java
+++ src/com/hci_420_620/ComputerSushiInteraction/LandingScreen.java
@@ -1,18 +1,17 @@
 package com.hci_420_620.ComputerSushiInteraction;
 
-import com.hci_420_620.ComputerSushiInteraction.R;
-
-import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.hci_420_620.computersushiinteraction.R;
+
 
 public class LandingScreen extends ActionBarActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.landing_screen);
