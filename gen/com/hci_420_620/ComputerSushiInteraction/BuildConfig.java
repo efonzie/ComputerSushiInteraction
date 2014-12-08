ebbc2abfc23e9d0614b40c774b76e7fd68632a30
Sam Tregillus <sam.tregillus@gmail.com>
2014-12-07 20:33:11 -0800

    fixed gitignore

--- gen/com/hci_420_620/ComputerSushiInteraction/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.hci_420_620.ComputerSushiInteraction;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
