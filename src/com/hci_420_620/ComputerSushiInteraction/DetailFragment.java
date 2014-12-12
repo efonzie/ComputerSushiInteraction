a8d25b6dbd7867e129947e224bbe8ecb6808fe5e
Ethan Matthews <Efonzie@gmail.com>
2014-12-12 08:38:24 -0800

    Added "call server"and "drink refill"buttons to the details fragment

--- src/com/hci_420_620/ComputerSushiInteraction/DetailFragment.java
+++ src/com/hci_420_620/ComputerSushiInteraction/DetailFragment.java
@@ -22,8 +22,8 @@ public class DetailFragment extends Fragment{
 		
 		
 		//Handle the button click, if you use android:onClick in the XML it will be sent to the activity
-		Button b = (Button)view.findViewById(R.id.addItemToOrder);
-		b.setOnClickListener(new OnClickListener(){
+		Button addToOrderButton = (Button)view.findViewById(R.id.addItemToOrder);
+		addToOrderButton.setOnClickListener(new OnClickListener(){
 			
 			@Override
 			public void onClick(View v){
@@ -32,9 +32,42 @@ public class DetailFragment extends Fragment{
 			
 		});
 		
+		//Handle the button click, if you use android:onClick in the XML it will be sent to the activity
+		Button drinkRefillButton = (Button)view.findViewById(R.id.drinkRefill);
+		drinkRefillButton.setOnClickListener(new OnClickListener(){
+			
+			@Override
+			public void onClick(View v){
+				drinkRefillButtonCLick(v);
+			}
+			
+		});
+		
+		//Handle the button click, if you use android:onClick in the XML it will be sent to the activity
+		Button callServerButton = (Button)view.findViewById(R.id.callServer);
+		callServerButton.setOnClickListener(new OnClickListener(){
+			
+			@Override
+			public void onClick(View v){
+				callServerButtonClick(v);
+			}
+			
+		});
+		
 		return view;
 	}
 	
+	// output message to indicate server is on their way
+	protected void callServerButtonClick(View v) {
+		Toast.makeText(getActivity(), "Your server will be with you shortly.", Toast.LENGTH_SHORT).show();
+	}
+
+	// output message to indicate server is coming to refill your drink
+	protected void drinkRefillButtonCLick(View v) {
+		Toast.makeText(getActivity(), "Your drink refill request has been sent to your server.", Toast.LENGTH_SHORT).show();
+	}
+
+	// update the menu details section for the newly selected item
 	public void updateDetails(MenuItem item){
 		selectedItem = item;
 		int imageId = item.getImageId();
@@ -63,7 +96,7 @@ public class DetailFragment extends Fragment{
 		itemDescView.setText(itemDesc);
 	}
 	
-	
+	// adds the currently selected item to the current order
 	public void addToOrderButton(View view){
 		FragmentManager fragMan = getActivity().getSupportFragmentManager();
 		String currentOrderTag = OrderPagerAdapter.GetFragmentTag(R.id.orderViewPager, 0);
